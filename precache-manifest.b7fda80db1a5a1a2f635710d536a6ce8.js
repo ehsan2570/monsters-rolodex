8f118728a595862f6c4252907a0d87f784dae909
ehsan2570 <ghandiehsan25@gmail.com>
2019-12-08 18:35:25 +0330

    Updates

---
+++ precache-manifest.b7fda80db1a5a1a2f635710d536a6ce8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63b68df6d3f830d14194eca0d6ae0a23",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "396d4a8f7963bb84db9d",
+    "url": "/monsters-rolodex/static/css/main.329a35c2.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "396d4a8f7963bb84db9d",
+    "url": "/monsters-rolodex/static/js/main.fa852810.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
